298dd88076a776ba974e31da31d55ce8f542313f
Albert <alhafram@gmail.com>
2024-08-26 17:32:13 +0300

    Initial Commit

---
+++ MetricsEstimatorDemo/MetricsEstimatorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetricsEstimatorDemoApp.swift
+//  MetricsEstimatorDemo
+//
+//  Created by Albert on 26.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MetricsEstimatorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
