2e0866388627f9b2a1fc80f0727f6d270faee258
Albert <alhafram@gmail.com>
2024-08-27 23:44:16 +0300

    Initial

---
+++ MetricsEstimatorDemo/PackageUsage.swift
@@ -0,0 +1,13 @@
+//
+//  PackageUsage.swift
+//  MetricsEstimatorDemo
+//
+//  Created by Albert on 26.08.2024.
+//
+
+import Foundation
+import LocalPackage
+
+func test() {
+    
+}
---
+++ MetricsEstimatorDemo/Packages/LocalPackage/Sources/LocalPackage/Model.swift
@@ -0,0 +1,26 @@
+import TestPackage1
+
+public struct StructModel {
+
+    public init() { }
+}
+
+public enum EnumModel {
+
+}
+
+open class OpenClassModel {
+    public init() { }
+}
+
+public class PublicClassModel {
+    public init() { }
+}
+
+public actor ActorModel {
+    public init() { }
+}
+
+public protocol ProtocolModel {
+    
+}
