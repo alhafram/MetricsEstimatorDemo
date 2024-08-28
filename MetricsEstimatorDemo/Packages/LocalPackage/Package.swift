76ea77b47c595acf3abdcfa1d92c9c2116974534
Albert <alhafram@gmail.com>
2024-08-28 09:54:20 +0300

    Add package

--- MetricsEstimatorDemo/Packages/LocalPackage/Package.swift
+++ MetricsEstimatorDemo/Packages/LocalPackage/Package.swift
@@ -14,7 +14,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/alhafram/TestPackage1.git", from: "1.0.0")
+        .package(url: "https://github.com/alhafram/TestPackage1.git", branch: "main"),
+        .package(url: "https://github.com/alhafram/TestPackage2.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -22,7 +23,8 @@ let package = Package(
         .target(
             name: "LocalPackage",
             dependencies: [
-                .product(name: "TestPackage1", package: "TestPackage1")
+                .product(name: "TestPackage1", package: "TestPackage1"),
+                .product(name: "TestPackage2", package: "TestPackage2"),
             ]
         )
     ]
